12a61f1932ff596b7c73bbbd54293634bdb95a3e
zhupengpeng <zhupp034@163.com>
2022-05-13 16:57:29 +0800

    feat: 避免了首次更新两次的问题

--- src/storeCom/storeCom1.tsx
+++ src/storeCom/storeCom1.tsx
@@ -8,20 +8,26 @@ function StoreCom1({hook, onUpdate}) {
   const initialLoad = useRef(false);
   console.log({data});
 
-  // 首次执行时立刻返回初始值
+  // 首次执行时立刻返回初始值，比useEffect要早，只执行一次，后面的交给useEffect
   useMemo(() => {
+    console.log('useMemo');
+    
     onUpdate(data)
     // updateRef.current(data)
     
-    // initialLoad.current = false;
+    initialLoad.current = false;
   }, []);
+
+  
   useEffect(() => {
-    onUpdate(data)
-    // if (initialLoad.current) {
-    //   updateRef.current(data)
-    // } else {
-    //   initialLoad.current = true;
-    // }
+    console.log('useEffect');
+    // 后面的更新才会执行到，首次不更新
+    if (initialLoad.current) {
+      onUpdate(data)
+      // updateRef.current(data)
+    } else {
+      initialLoad.current = true;
+    }
  
   })
   
